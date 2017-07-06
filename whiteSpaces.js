8b4fa40517c67efda7e5e362da69fb4fdb97ca12
tarunnagpal <tarun.nagpal@nagarro.com>
2017-07-06 16:53:56 +0530

    Whitespaces

---
+++ whiteSpaces.js
@@ -0,0 +1,22 @@
+var 
+
+// first name of the person
+firstName,
+
+// last name of the person
+lastName,
+
+// language of the person
+language;
+
+var person = {
+
+  // firstName of the person
+  firstName : "Tarun",
+
+  // LastName of the person
+  // Support multiple comments
+  lastName : "Nagpal",
+};
+
+console.log(person);
