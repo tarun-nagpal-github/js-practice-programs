c1f9cb5f6b5d086c14244f0f01942ddcf2b5cc32
tarunnagpal <tarun.nagpal@nagarro.com>
2017-07-06 16:42:36 +0530

    updating

--- semicolon.js
+++ semicolon.js
@@ -1,5 +1,8 @@
 /**
  * Demonstrating the disadvantage of automatic semicolon 
+ * 
+ * After the return statement, an automtic semicolon will be added.
+ * This will return the undefined object rather than the actual object * 
  */
 function greet(){
    return  
