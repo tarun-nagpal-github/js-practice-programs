da8cddb9eea0a1e2fc47c9d801fc62c483745901
tarunnagpal <tarun.nagpal@nagarro.com>
2017-07-26 19:40:08 +0530

    added

--- arrayFunctions.js
+++ arrayFunctions.js
@@ -1,3 +1,22 @@
+
+// **************** Concat Function *******************************
+var myString = 'Hello World. How are you doing?';
+var myNumbers = [1,2,3,4,5,7];
+myNumbers.concat(myString);
+
+
+// ****************** Filter function ****************************
+var rollNo = [1,2,3,4,5,6,7,8,9,10];
+function getEven(number) {
+   return number % 2 === 0;  
+}
+rollNo.filter(getEven);
+
+// ****************** Index Of ****************************
+var myNewArray = ['One', 'Two'];
+myNewArray.indexOf('Two')
+
+
 /**
  * Reverse an Array
  * @param {*} array 
@@ -30,16 +49,6 @@ console.log(reverseArray(["A", "B", "C"]));
 console.log(arrayValue);
 
 
-// Filter function
 
-// Actual array 
-var rollNo = [1,2,3,4,5,6,7,8,9,10];
 
-/**
-* Condition function 
-*/
-function getEven(number) {
-   return number % 2 === 0;  
-}
-// Filter function implemented 
-rollNo.filter(getEven);
+
